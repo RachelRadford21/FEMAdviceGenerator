f473e9dd8a3ccf60bd42ceb7162da1fe665f8072
Rachel Radford <rachelradford21@gmail.com>
2022-06-14 17:00:13 -0400

    Initial Commit

---
+++ FEMAdviceGenerator/FEMAdviceGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FEMAdviceGeneratorApp.swift
+//  FEMAdviceGenerator
+//
+//  Created by Rachel Radford on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct FEMAdviceGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
